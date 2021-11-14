4143ffb8675b9af0e8b9744c5e886d073d6d2221
nadiaalamgir21 <87517265+nadiaalamgir21@users.noreply.github.com>
2021-11-13 18:12:11 -0800

    initial working project completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,35 @@
 let apiId = "a248938f43b3027a2f331d88df42b72b"
-let url1 = `https://api.openweathermap.org/data/2.5/weather?q=sacramento&APPID=${apiId}&units=imperial`
-
+let area = "sacramento"
+let url1 = `https://api.openweathermap.org/data/2.5/weather?q=${area}&APPID=${apiId}&units=imperial`
 let data = null
 let futureData = null
 
-function init() {
+function callFirstAPI(url) {
   let whenDataArrives = function (response) {
     response.json().then((dataFromApi) => {
       console.log(dataFromApi)
       data = dataFromApi
       displayOnScreen()
       fetchFutureWeather(dataFromApi)
+      saveCity(dataFromApi.name)
     })
   }
-  fetch(url1).then(whenDataArrives)
+  fetch(url).then(whenDataArrives)
 }
 
-$(document).ready(init)
+$(document).ready(() => {
+
+  getStoredCities()
+  callFirstAPI(url1)
+})
 
 function displayOnScreen() {
   $(".current-day-weather").html(
     ` <h2>${data.name}</h2>
+    <h5>${moment.unix(data.dt).format("DD/MM/YYYY")}</h5>
+        <p><img src="http://openweathermap.org/img/w/${
+          data.weather[0].icon
+        }.png"</img></p>
         <p>Temp: ${data.main.temp} <span>&#176;</span>F</p>
         <p>Wind ${data.wind.speed}MPH</p>
         <p>Humidity ${data.main.humidity}%</p>
@@ -50,14 +59,12 @@ function printFutureWeather(data) {
   let appendStr = ""
 
   for (let i = 0; i < 5; i++) {
-    // console.log(moment(daily[i].dt).format('MM/DD/YYYY'))
-    console.log(Date(daily[i].dt))
-    console.log("")
-
-
+    // console.log( moment.unix(data.daily[i].dt).format('DD/MM/YYYY'))
     appendStr += `<div>
-                  <h5>${daily[i].dt}</h5>
-                  <p>${daily[i].weather[0].icon}</p>
+                  <h5>${moment.unix(data.daily[i].dt).format("DD/MM/YYYY")}</h5>
+                  <img src="http://openweathermap.org/img/w/${
+                    daily[i].weather[0].icon
+                  }.png"</img>
                   <p>Temp: ${daily[i].temp.day} <span>&#176;</span>F</p>
                   <p>Wind ${daily[i].wind_speed}MPH</p>
                   <p>Humidity ${daily[i].humidity}%</p>
@@ -66,3 +73,55 @@ function printFutureWeather(data) {
 
   $(".weather-cards-wrapper").html(appendStr)
 }
+
+function handleSearch(city) {
+  let area = null
+  if (city) {
+    area = city
+  }else{
+    area = $("#searchBar").val()
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&APPID=${apiId}&units=imperial`
+  callFirstAPI(url)
+}
+
+function saveCity(city) {
+  console.log("city", city)
+
+
+  let cities = JSON.parse(localStorage.getItem('cityArr'))
+  if(cities.indexOf(city) === -1){ 
+  
+    cities.push(city)
+    localStorage.setItem('cityArr', JSON.stringify(cities))
+    getStoredCities()
+   
+  }
+  
+}
+
+
+
+// localStorage.setItem('cityArr', JSON.stringify(['Miami', 'Nevada', 'Utah']))
+
+function getStoredCities() {
+  if (localStorage.getItem("cityArr") === null){
+    localStorage.setItem('cityArr', JSON.stringify([]))
+    return
+  } 
+  let cities = JSON.parse(localStorage.getItem("cityArr"))
+
+  let str = ""
+  for (let i = 0; i < cities.length; i++) {
+    str += `<button class="search-btn" onclick="handleSearch('${cities[i]}')">${cities[i]}</button>`
+  }
+  
+  $(".savedCities").html(str)
+}
+
+//todo initial load main localstorage main stored list ko fetch karna hai DONE
+//and to show on the screen DONE
+//todo when saving localstorage main stored lost main duplication avoid
+//if there is no duplication we will push to the list
+//if the city already exist we will not the push to the list
